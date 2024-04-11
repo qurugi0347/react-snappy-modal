49e5e14d91c554ae1ddf009483bd492e8741ad4e
Jun Seo <qurugi0347@gmail.com>
2024-04-12 00:47:14 +0900

    개발 환경 세팅 (#1)
    * FEAT: .gitignore 추가
    
    * FEAT: nvmrc 추가
    
    * FEAT: package setting
    
    * FEAT: vite를 사용한 lib 개발 세팅
    
    * FEAT: husky setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+}
