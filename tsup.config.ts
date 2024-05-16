8ec1027b216ae032cb290392d8ef9851180f745e
Jun Seo <qurugi0347@gmail.com>
2024-05-16 20:40:09 +0900

    ESM 모듈 빌드 오류 수정 (#14)
    * v0.0.4
    
    * FIX: npmignore
    
    - mts, mjs allow
    
    * v0.0.5
    
    * FIX: ESM Setting
    
    * FEAT: Build ESM

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   splitting: false,
   sourcemap: true,
   clean: true,
+  external: ["react", "react-dom"], // 외부 모듈 설정
   format: ["cjs", "esm"],
+  cssModules: true,
   dts: true,
+  loader: {
+    ".css": "css", // CSS 파일 로더 설정
+  },
 });
