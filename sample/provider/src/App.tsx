7c1f0c61423d102af61989dd67cdbb2475f5829c
Jun <qurugi0347@gmail.com>
2024-08-28 22:09:01 +0900

    CHORE: TMP

--- sample/provider/src/App.tsx
+++ sample/provider/src/App.tsx
@@ -1,35 +1,9 @@
-import "./App.css";
-import SnappyModal from "../../../src";
-
 function App() {
-  const handleShowModal = () => {
-    SnappyModal.show(<ModalComponent />)
-      .then((message: string) => {
-        console.log("Modal closed", message);
-      })
-      .catch((message: string) => {
-        console.log("Modal thrown", message);
-      });
-  };
-
   return (
     <>
       <h1>React Snappy Modal</h1>
-      <button onClick={handleShowModal}>Test Modal</button>
     </>
   );
 }
 
 export default App;
-
-const ModalComponent = () => {
-  return (
-    <div style={{ background: "white" }}>
-      <h1>Modal</h1>
-      <button onClick={() => SnappyModal.close("confirmObject")}>
-        confirm
-      </button>
-      <button onClick={() => SnappyModal.throw("throwObject")}>throw</button>
-    </div>
-  );
-};
