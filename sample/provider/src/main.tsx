efe06fffab7e39224f66faec230acc2274cc95ce
Jun <qurugi0347@gmail.com>
2024-09-01 18:46:56 +0900

    CHORE: export 수정

--- sample/provider/src/main.tsx
+++ sample/provider/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BookProvider } from "./book/BookContext.tsx";
-import { SnappyModalProvider } from "../../../src/context/SnappyModalContext.tsx";
+import { SnappyModalProvider } from "../../../src";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
